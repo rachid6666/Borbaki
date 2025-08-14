5ca3719f693069375e899fd8a606862b0fbbbf39
yg <yahia@Gmail.com>
2025-08-14 20:23:30 +0100

    Make Navbar responsive, remove underline from BorBaki title, update CSS

--- Client/src/components/navbar/Navbar.jsx
+++ Client/src/components/navbar/Navbar.jsx
@@ -1,43 +1,43 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
-import { useNavigate } from 'react-router-dom';
-
 
 const Navbar = () => {
-
-    
-
+  const [open, setOpen] = useState(false);
   const { user, logout } = useContext(AuthContext);
   const navigate = useNavigate();
+
   const handleLogout = () => {
-   
-    logout(); 
+    logout();
     navigate("/login");
-    
   };
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" className="no-underline">
           <span className="logo">BorBaki</span>
         </Link>
+
+        {/* Hamburger (only shows on mobile) */}
+        <div className="hamburger" onClick={() => setOpen(!open)}>
+          &#9776;
+        </div>
+
         {user ? (
-          <>
-            <div className="navUser">
-              <Link to="/account" className="no-underline">
-                <span className="user-nav">Marhaba, {user.username}</span>
-              </Link>
-              <button className="navButton" onClick={handleLogout}>
-                Logout
-              </button>{" "}
-            </div>
-          </>
+          <div className={`navItems ${open ? "active" : ""}`}>
+            <Link to="/account" className="no-underline">
+              <span className="user-nav">Marhaba, {user.username}</span>
+            </Link>
+            <button className="navButton" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
         ) : (
-          <div className="navItems">
-             <div className="navItems-center">
-             <Link to="/">
+          <div className={`navItems ${open ? "active" : ""}`}>
+            <div className="navItems-center">
+              <Link to="/">
                 <button className="navButtonHome">Discover</button>
               </Link>
               <Link to="/car-rental">
