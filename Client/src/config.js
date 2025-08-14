053e1278e6ebe46134928f99860ea0bd1490dc4a
Yahia Gadouche <djooker309@gmail.com>
2025-08-14 14:32:45 +0100

    Update config.js

--- Client/src/config.js
+++ Client/src/config.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL || "https://borbaki.onrender.com";
 
-export default axios.create({
+const api = axios.create({
   baseURL: `${API_URL}/api`,
 });
+
+export default api;
